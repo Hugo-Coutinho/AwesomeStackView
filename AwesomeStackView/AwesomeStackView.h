fba2c19b518df8f3791890caf955c5a02769a72e
Rogerinho <hugocoutinho@brq.com>
2020-04-03 13:40:11 -0300

    first commit

---
+++ AwesomeStackView/AwesomeStackView.h
@@ -0,0 +1,19 @@
+//
+//  AwesomeStackView.h
+//  AwesomeStackView
+//
+//  Created by BRQ on 03/04/20.
+//  Copyright © 2020 Hugo Coutinho. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AwesomeStackView.
+FOUNDATION_EXPORT double AwesomeStackViewVersionNumber;
+
+//! Project version string for AwesomeStackView.
+FOUNDATION_EXPORT const unsigned char AwesomeStackViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AwesomeStackView/PublicHeader.h>
+
+
